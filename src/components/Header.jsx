00f106b841ddb5a71a9b8bfab210f79e2829bad7
Matilda-bit <polinaovras@gmail.com>
2024-04-19 22:44:19 +0300

    css modules experience

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import logo from '../assets/logo.png';
+//import './Header.css';
+import clsses from './Header.module.css';//Vanilla CSS
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={clsses.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
